b3ff58cd68cf3ca0f467436814dc9c7fa1fb9092
Warren Smith <warrengsmith@yahoo.com>
2016-08-06 18:56:44 -0400

    Updates to disallow uppercase UUID alphabetic characters and add Groovydoc

--- GroovyExamples/src/main/groovy/com/jalamor/example/ValidateUUID.groovy
+++ GroovyExamples/src/main/groovy/com/jalamor/example/ValidateUUID.groovy
@@ -1,9 +1,20 @@
 package com.jalamor.example
 
+/**
+ * <p>Class ValidateUUID validates a type 4 UUID. In its canonical form, a UUID is represented by 32 lowercase hexadecimal digits, 
+ * displayed in five groups separated by hyphens, in the form 8-4-4-4-12 for a total of 36 characters (32 alphanumeric characters
+ * and four hyphens).</p>
+ * <p>For example:<br/><br/>
+    <code>123e4567-e89b-12d3-a456-426655440000</code>
+    <p/> 
+ *
+ * @author Warren Smith
+ * @version 1.0
+ */
 class ValidateUUID {
 
 	static boolean isValid(final String uuid) {
-		def matcher = uuid =~ /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/
+		def matcher = uuid =~ /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
 		return matcher.matches()
 	}
 }
--- GroovyExamples/src/test/groovy/com/jalamor/example/ValidateUUIDSpec.groovy
+++ GroovyExamples/src/test/groovy/com/jalamor/example/ValidateUUIDSpec.groovy
@@ -3,6 +3,12 @@ package com.jalamor.example
 import spock.lang.Specification
 import spock.lang.Unroll;
 
+/**
+ * Spock test class for ValidateUUID
+ *
+ * @author Warren Smith
+ * @version 1.0
+ */
 class ValidateUUIDSpec extends Specification {
 
 	@Unroll
@@ -13,9 +19,13 @@ class ValidateUUIDSpec extends Specification {
 		  
 		where:
 		uuid  									|| result
-		'148g5327-b297-44bc-8ccc-46f8fd4c32e6'  || false
-		'148f5327-b297-44bc-8ccc-46f8fd4c32e6'  || true
-		'148f5327xb297-44bc-8ccc-46f8fd4c32e6'  || false
 		'148f5327-b297-44bc-8ccc-46f8fd4c32f6'	|| true
+		'148f5327-b297-44bc-8ccc-46f8fd4c32e6'  || true
+		'123e4567-e89b-12d3-a456-426655440000'	|| true
+		'148f5327-B297-44bc-8ccc-46f8fd4c32e6'  || false // on of the characters is uppercase which is invalid
+		'148g5327-b297-44bc-8ccc-46f8fd4c32e6'  || false // one of the characters is g which is invalid
+		'148f5327xb297-44bc-8ccc-46f8fd4c32e6'  || false // on of the dashes replaced with x is invalid
+		'148f5327-b297-44bc-8ccc-46f8fd4c32e'   || false // too short
+		'148f5327-b297-44bc-8ccc-46f8fd4c32e66' || false // too long
 	}
 }
